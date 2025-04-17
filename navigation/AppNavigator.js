c1cca3a9b0b7f53f5a4419e5e80d458f4140c3bf
rouscathe <1001.26121637.ucla@gmail.com>
2025-04-17 15:38:19 -0400

    actualizacion de navigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,9 +2,11 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Start from "../app/Start";
 import Login from "../app/Login";
+import Register from "../app/Register";
 import NewPassword from "../app/NewPassword";
 import RecoverPassword from "../app/RecoverPassword";
 import RecoverPasswordCode from "../app/RecoverPasswordCode";
+import RegisterUpload from "../app/RegisterUpload";
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
@@ -15,11 +17,13 @@ export default function AppNavigator() {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="recoverPassword" component={RecoverPassword} />
         <Stack.Screen name="NewPassword" component={NewPassword} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="RegisterUpload" component={RegisterUpload} />
         <Stack.Screen
           name="RecoverPasswordCode"
           component={RecoverPasswordCode}
         />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  );
 }
